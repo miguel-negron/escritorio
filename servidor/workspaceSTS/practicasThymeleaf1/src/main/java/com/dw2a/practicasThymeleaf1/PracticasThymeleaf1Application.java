5cb7ff9b9af834aff9b5fd8a3710a788b9a111e6
miguel <miguel.negron@gmail.com>
2020-10-02 12:59:16 +0100

    a ver donde se añade esto

---
+++ servidor/workspaceSTS/practicasThymeleaf1/src/main/java/com/dw2a/practicasThymeleaf1/PracticasThymeleaf1Application.java
@@ -0,0 +1,13 @@
+package com.dw2a.practicasThymeleaf1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasThymeleaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasThymeleaf1Application.class, args);
+	}
+
+}
---
+++ servidor/workspaceSTS/practicasThymeleaf1/src/test/java/com/dw2a/practicasThymeleaf1/PracticasThymeleaf1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw2a.practicasThymeleaf1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasThymeleaf1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
