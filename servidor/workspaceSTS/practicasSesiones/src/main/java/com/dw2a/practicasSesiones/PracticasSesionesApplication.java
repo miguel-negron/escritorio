a586bc3c5d2b3d707090fc8aa5ec0467756ea578
miguel <miguel.negron@gmail.com>
2020-10-16 09:32:17 +0200

    nuevo proyecto de sesiones

---
+++ servidor/workspaceSTS/practicasSesiones/src/main/java/com/dw2a/practicasSesiones/PracticasSesionesApplication.java
@@ -0,0 +1,13 @@
+package com.dw2a.practicasSesiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasSesionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasSesionesApplication.class, args);
+	}
+
+}
---
+++ servidor/workspaceSTS/practicasSesiones/src/test/java/com/dw2a/practicasSesiones/PracticasSesionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw2a.practicasSesiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticasSesionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
